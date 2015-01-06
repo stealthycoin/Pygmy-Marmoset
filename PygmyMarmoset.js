adb5f69d0c90a35dbcaa34c948d38821ba7a0624
stealthycoin <john.w.carlyle@gmail.com>
2015-01-06 15:11:40 -0800

    get_blob for uploading base64Data

--- PygmyMarmoset.js
+++ PygmyMarmoset.js
@@ -230,6 +230,24 @@ var PygmyMarmoset = (function () {
             result_image.src = new_canvas.toDataURL(encoding);
         },
 
+        get_blob: function (callback, encoding) {
+            PygmyMarmoset.get_data(function(img) {
+                var dataURI = img.src;
+                var byteString = window.atob(dataURI.split(',')[1]);
+
+                var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+                var ab = new ArrayBuffer(byteString.length);
+                var ia = new Uint8Array(ab);
+                for (var i = 0; i < byteString.length; i++) {
+                    ia[i] = byteString.charCodeAt(i);
+                }
+
+                var bb = new Blob([ab], { "type": mimeString });
+                callback(bb);
+            }, encoding);
+        },
+
         toggle_zoom: function(value) {
             if (showing_zoom !== value) {
                 showing_zoom = value;
